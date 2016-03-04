dd364e907d9b5e6c89b1d174b53ee46142c08fdf
Noobulater <AmanBentley@gmail.com>
2016-03-04 07:35:52 -0500

    Generation 2

--- kernel.cu
+++ kernel.cu
@@ -18,13 +18,22 @@ int timeStep;
 
 int N = 100;
 int S = 1; // STEPS
+bool logStamp;
+bool logInit;
+bool logChanges;
 
 void loadOptions() {
 	// Loads up the config.ini file, and extracts parameters
+	printf("Loading Options... \n", N);
+
+	logStamp = true;
+	logInit = true;
+	logChanges = true;
+
 	CSimpleIniA ini;
 	ini.SetUnicode();
 	ini.LoadFile("config.ini");
-
+	printf("Completed! \n\nParameters \n", N);
 	// Size paramater
 	const char * value = ini.GetValue("General", "n", "0");
 
@@ -34,7 +43,7 @@ void loadOptions() {
 	}
 	else {
 		N = atoi(value);
-		printf("Loaded N = %d \n", N);
+		printf(" N = %d \n", N);
 	}
 
 	// Size paramater
@@ -46,8 +55,18 @@ void loadOptions() {
 	}
 	else {
 		S = atoi(steps);
-		printf("Loaded timeSteps = %d \n", S);
+		printf(" TimeSteps = %d \n", S);
 	}
+
+	const char *sizestamp = ini.GetValue("Logs", "sizestamp", "yes");
+	if (sizestamp[0] == 'n' || sizestamp[0] == '0' || sizestamp[0] == 'f' || sizestamp[0] == 'F')
+		logStamp = false;
+	const char *loginit = ini.GetValue("Logs", "loginit", "yes");
+	if (loginit[0] == 'n' || loginit[0] == '0' || loginit[0] == 'f' || loginit[0] == 'F')
+		logInit = false;
+	const char *logchanges = ini.GetValue("Logs", "logchanges", "yes");
+	if (logchanges[0] == 'n' || logchanges[0] == '0' || logchanges[0] == 'f' || logchanges[0] == 'F')
+		logChanges = false;
 }
 
 void pullSpecs(int *blocks, int *threads) {
@@ -64,35 +83,41 @@ void pullSpecs(int *blocks, int *threads) {
 			{
 				*blocks = properties.multiProcessorCount;
 				*threads = properties.maxThreadsPerMultiProcessor;
+				printf("Success!\n\n");
 			}
 	}
 }
 
-__global__ void processChunk(char *value, char* oldFrame, int N, int seed) {
+__global__ void processChunk(char *value, char* oldFrame, int bm, int br, int tm, int tr, int N, int seed) {
 	//int sIndex = threadIdx.x + blockIdx.x * blockDim.x;
-	int bm = N/gridDim.x;
-	int br = N % gridDim.x;
-	
+	int b = bm;
 	if (br > 0 && blockIdx.x < br) {
-		bm = bm + 1;
+		b++;
+	}
+
+	int t = tm;
+	if (tr > 0 && threadIdx.x < tr) {
+		t++;
 	}
 
 	curandState_t state;
 
-	curand_init(seed, 0, 0, &state);
+	curand_init((threadIdx.x + blockIdx.x) * seed / blockIdx.x, 0, 0, &state);
 
-	for (int i = 0; i < bm; i++) {
-		for (int x = 0; x < N; x++) {
-			for (int y = 0; y < N; y++) {
-				int index = x + y * N + (gridDim.x * i + blockIdx.x) * N * N;
+	for (int z = 0; z < b; z++) {
+		int y = 0;
+		for (int x = 0; x < t; x++) {
+			for (int y = 0; y < t; y++) {
+				int index = (blockDim.x * x + threadIdx.x % N) + (blockDim.x * y + threadIdx.x / N) * N + (gridDim.x * z + blockIdx.x) * N * N;
 				if (index < N*N*N) { 
 					// sanity check
 					int newValue = curand(&state) % 3;
 					//Use old values to determine probablility of mutation
 					//if (value[index] != newValue) {
-					//	value[index] = newValue;
+						//value[index] = newValue;
+						value[index] = 5;
 					//}
-					value[index] = blockIdx.x + 5;
+					//value[index] = blockIdx.x + 5;
 				}
 			}
 		}
@@ -104,8 +129,18 @@ __global__ void processChunk(char *value, char* oldFrame, int N, int seed) {
 // 00 / 01 / 10 / 11
 int main(int argc, char *args[])
 {
+	// Load Specs
+	int blocks, threads;
+	printf("Retreiveing Hardware Specifications... \n");
+
+	pullSpecs(&blocks, &threads);
+	printf("multiProcessorCount %d\n", blocks);
+	printf("maxThreadsPerMultiProcessor %d\n", threads);
+	printf("\n");
+	
 	// Load Options
 	loadOptions();
+	printf("\n");
 
 	// Adds numbers and stuff
 	int SIZE = N*N*N; // N^3 Size
@@ -120,11 +155,19 @@ int main(int argc, char *args[])
 	int check = mkdir("outputs");
 
 	char buff[256];
-	sprintf(buff, "outputs/InitState_%d.csv", N);
+	if (logStamp)
+		sprintf(buff, "outputs/InitState_%d.csv", N);
+	else
+		sprintf(buff, "outputs/InitState.csv", N);
 
-	FILE* output = fopen(buff, "w");
-	fputs("X,Y,Z,CELLTYPE\n", output);
+	FILE* output;
+	if (logInit) {
+		output = fopen(buff, "w");
+		fputs("X,Y,Z,CELLTYPE\n", output);
+	}
 	srand(time(NULL));
+
+	printf("Generating Initial Population\n");
 	//a contains all value
 	for (int i = 0; i < SIZE; i++) {
 		// if healthy, glia = 10, normal = 00, cancer glia = 11, cancer = 01
@@ -132,46 +175,46 @@ int main(int argc, char *args[])
 		a[i] = (rand() % 2) * (g+g); // everything starts as a healthy cell
 		// This is where cell structure will be defined
 
-		fprintf(output, "%d,%d,%d,%d \n", i % N, (i / N) % N, i / (N * N), a[i]);
+		if (logInit)
+			fprintf(output, "%d,%d,%d,%d\n", i % N, (i / N) % N, i / (N * N), a[i]);
 	}
 	// write the initial state
-	fflush(output);
-	fclose(output);
+	if (logInit)
+		fclose(output);
 	 
+	printf("Success\n\n");
+
 	char c_buff[256];
-	sprintf(c_buff, "outputs/ChangeState_%d.csv", N);
-	c_output = fopen(c_buff, "w");
+	if (logStamp)
+		sprintf(c_buff, "outputs/ChangeState_%d.csv", N);
+	else 
+		sprintf(c_buff, "outputs/ChangeState.csv", N);
+	if (logChanges) {
+		c_output = fopen(c_buff, "w");
+		fputs("X,Y,Z,CELLTYPE,TIMESTEP\n", c_output);
+	}
 
-	fputs("X,Y,Z,CELLTYPE,TIMESTEP\n", c_output);
-
-	int blocks, threads;
-
-	pullSpecs(&blocks, &threads);
-	printf("multiProcessorCount %d\n", blocks);
-	printf("maxThreadsPerMultiProcessor %d\n", threads);
-
+	printf("Transferring Data to GPU\n");
 	cudaMalloc(&d_a, SIZE*sizeof(char));
 	cudaMalloc(&d_init, SIZE*sizeof(char));
 	// have to load initial state, there is no way around it
 	cudaMemcpy(d_a, a, SIZE*sizeof(char), cudaMemcpyHostToDevice); 
 	cudaMemcpy(d_init, a, SIZE*sizeof(char), cudaMemcpyHostToDevice);
+	printf("Success\n\n");
 
-	int s = N / 2;
-	// ODD number so it has a remainder
-	if (N % 2) { 
-		s = s + 1;
-	}
+	int blockCount = min(N, blocks);
+	int threadCount = min(N*N, threads * 13 / 19);
 
-	// Responsibility, #of layers this thread will process
-	int blockCount = blocks;
+	int bm = N / blockCount;
+	int br = N % blockCount;
 
-	printf("%d %d \n", s, blockCount);
+	int tm = N*N / threadCount;
+	int tr = N*N % threadCount;
 
+	printf("Crunching Data\n", tm, tr);
 	for (int timeStep = 0; timeStep < S; timeStep++) {
 		int seed = rand();
-		processChunk <<<blockCount, 1>>>(d_a, d_init, N, seed);
-		cudaDeviceSynchronize(); // Force Kernels to complete
-
+		processChunk <<<blockCount, threadCount>> >(d_a, d_init, bm, br, tm, tr, N, seed);
 		cudaDeviceSynchronize(); // Force Kernels to complete
 
 		cudaMemcpy(n_a, d_a, SIZE*sizeof(char), cudaMemcpyDeviceToHost);
@@ -180,17 +223,20 @@ int main(int argc, char *args[])
 			// if healthy, glia = 10, normal = 00, cancer glia = 11, cancer = 01
 			if (a[i] != n_a[i])  {
 				// only record differences, also update a[i]
-				fprintf(c_output, "%d,%d,%d,%d,%d \n", i % N, (i / N) % N, i / (N * N), n_a[i] - 5, timeStep);
 				a[i] = n_a[i];
 				// Send changes back to the GPU
+				if (logChanges)
+					fprintf(c_output, "%d,%d,%d,%d,%d\n", i % N, (i / N) % N, i / (N * N), n_a[i], timeStep);
 				cudaMemcpy(&d_init[i], &a[i], sizeof(char), cudaMemcpyHostToDevice);
 			}
 		}
 	}
 
-	fflush(c_output);
-	fclose(c_output);
-		
+	if (logChanges)
+		fclose(c_output);
+	
+	printf("Simulation Completed\n");
+
 	while (true) {
 
 	}
